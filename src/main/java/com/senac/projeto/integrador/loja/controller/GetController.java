834d363154d88c32dd3e17635edefdbf57b2c57c
Danilo-Dassi-Araujo <danariano83@gmail.com>
2023-08-27 22:37:33 -0300

    login finalizado

--- src/main/java/com/senac/projeto/integrador/loja/controller/GetController.java
+++ src/main/java/com/senac/projeto/integrador/loja/controller/GetController.java
@@ -1,9 +1,12 @@
 package com.senac.projeto.integrador.loja.controller;
 
+import com.senac.projeto.integrador.loja.dto.request.LoginRequestDTO;
 import com.senac.projeto.integrador.loja.dto.response.ListingDTOResponse;
+import com.senac.projeto.integrador.loja.dto.response.LoginDTOResponse;
 import com.senac.projeto.integrador.loja.filter.ControllerFilter;
 import com.senac.projeto.integrador.loja.indicator.GroupIndicator;
 import com.senac.projeto.integrador.loja.service.ListingUsersService;
+import com.senac.projeto.integrador.loja.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +22,18 @@ import java.util.List;
 public class GetController {
 
     private final ListingUsersService listingUsersService;
+    private final LoginService loginService;
+
     @GetMapping("/listingUser")
     public ResponseEntity<List<ListingDTOResponse>> getFilteredUsers(ControllerFilter controllerFilter,
                                                                      @RequestParam GroupIndicator groupIndicator) throws Exception {
         List<ListingDTOResponse> allUsers = listingUsersService.getAllUsers(controllerFilter, groupIndicator);
         return ResponseEntity.ok().body(allUsers);
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<LoginDTOResponse> login(LoginRequestDTO loginRequestDTO) throws Exception {
+        LoginDTOResponse loginDTOResponse = loginService.loginUser(loginRequestDTO);
+        return ResponseEntity.ok().body(loginDTOResponse);
     }
 }
